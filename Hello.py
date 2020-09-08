e690a9a7e8167a1455fa7de7e4135a369fabd51e
Duko-H <70583016+Duko-H@users.noreply.github.com>
2020-09-08 04:10:02 -0700

    Hello you Python
    Dit is me Python code.

---
+++ Hello.py
@@ -0,0 +1,21 @@
+Hello = input("Hallo! ")
+print("Ik ben Duko. ")
+
+name = input("Wie ben jij? ")
+print("Hallo,", name)
+print("nice to meet you. ")
+
+hgh = input("Hoe gaat het met u? (goed/slecht) ")
+
+if hgh.lower() == "goed":
+   print("dat is goed om te horen. ")
+if hgh.lower() == "slecht":
+   print("Ohw dat is niet fijn, ik wens u beterschap. ")
+
+
+
+
+
+
+
+
